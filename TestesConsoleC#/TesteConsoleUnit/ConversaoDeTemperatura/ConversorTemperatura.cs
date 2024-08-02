cbc6519fbdb7b870843d1719cf6558abb2735e8d
Eduardo Merbach Brenn <44850641822@info.local>
2024-08-02 10:02:04 -0300

    02/08 - Criando método

---
+++ TestesConsoleC#/TesteConsoleUnit/ConversaoDeTemperatura/ConversorTemperatura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversaoDeTemperatura
+{
+    public static class ConversorTemperatura
+    {
+        // Método conversor de temperatura
+        public static double ConverterCelsiusParaFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+    }
+}
